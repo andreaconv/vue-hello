1714ab9ae61d9baabc72c81962e1eed4834bb92f
andreaconv <andrea2convertini@gmail.com>
2023-04-12 14:50:39 +0200

    end

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+const {createApp} = Vue;
+
+createApp({
+  //metodo data
+  data(){
+    //return dell'oggetto
+    return {
+      messaggio: "Ciao Vue",
+    }
+  },
+
+  methods: {
+
+  }
+
+}).mount('#app')
